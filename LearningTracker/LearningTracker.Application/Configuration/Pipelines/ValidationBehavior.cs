44a2871c9606633c782594b30c60061779d026a1
Azamat Salamat <azamat.salamat.s@gmail.com>
2025-09-05 11:51:46 +0500

    test(): add tests for RegisterUserCommand

--- LearningTracker/LearningTracker.Application/Configuration/Pipelines/ValidationBehavior.cs
+++ LearningTracker/LearningTracker.Application/Configuration/Pipelines/ValidationBehavior.cs
@@ -5,28 +5,28 @@ using MediatR;
 namespace LearningTracker.Application.Configuration.Pipelines;
 
 internal abstract class ValidationBehavior<TRequest, TResponse> : AbstractValidator<TRequest>,
-    IPipelineBehavior<TRequest, Result<TResponse>> 
+    IPipelineBehavior<TRequest, Result<TResponse>>
     where TRequest : IRequest<Result<TResponse>>
 {
     public async Task<Result<TResponse>> Handle(TRequest request, RequestHandlerDelegate<Result<TResponse>> next, CancellationToken ct)
     {
         var fluentValidationResult = await base.ValidateAsync(request, ct);
-        if (!fluentValidationResult.IsValid) 
+        if (!fluentValidationResult.IsValid)
         {
             var errorText = string.Join(Environment.NewLine, fluentValidationResult.Errors.Select(x => $"{x.ErrorMessage}"));
             return Result.Failure<TResponse>(errorText);
         }
 
         var requestValidateResultAsync = await RequestValidateAsync(request, ct);
-        if (requestValidateResultAsync.IsFailure) 
+        if (requestValidateResultAsync.IsFailure)
         {
             return Result.Failure<TResponse>(requestValidateResultAsync.Error);
         }
 
         return await next();
     }
-    
-    protected virtual Task<Result> RequestValidateAsync(TRequest request, CancellationToken ct) 
+
+    protected virtual Task<Result> RequestValidateAsync(TRequest request, CancellationToken ct)
     {
         return Task.FromResult(Result.Success());
     }
@@ -34,27 +34,27 @@ internal abstract class ValidationBehavior<TRequest, TResponse> : AbstractValida
 
 internal abstract class ValidationBehavior<TRequest> : AbstractValidator<TRequest>,
     IPipelineBehavior<TRequest, Result>
-    where TRequest : IRequest<Result> 
+    where TRequest : IRequest<Result>
 {
-    public async Task<Result> Handle(TRequest request, RequestHandlerDelegate<Result> next, CancellationToken ct) 
+    public async Task<Result> Handle(TRequest request, RequestHandlerDelegate<Result> next, CancellationToken ct)
     {
         var fluentValidationResult = await base.ValidateAsync(request, ct);
-        if (!fluentValidationResult.IsValid) 
+        if (!fluentValidationResult.IsValid)
         {
             var errorText = string.Join(Environment.NewLine, fluentValidationResult.Errors.Select(x => $"{x.ErrorMessage}"));
             return Result.Failure(errorText);
         }
 
         var requestValidateResultAsync = await RequestValidateAsync(request, ct);
-        if (requestValidateResultAsync.IsFailure) 
+        if (requestValidateResultAsync.IsFailure)
         {
             return Result.Failure(requestValidateResultAsync.Error);
         }
-        
+
         return await next();
     }
 
-    protected virtual Task<Result> RequestValidateAsync(TRequest request, CancellationToken ct) 
+    internal virtual Task<Result> RequestValidateAsync(TRequest request, CancellationToken ct)
     {
         return Task.FromResult(Result.Success());
     }
--- LearningTracker/LearningTracker.Application/UseCases/Auth/Register/RegisterUserCommandValidator.cs
+++ LearningTracker/LearningTracker.Application/UseCases/Auth/Register/RegisterUserCommandValidator.cs
@@ -6,23 +6,27 @@ using LearningTracker.Domain.Repositories;
 
 namespace LearningTracker.Application.UseCases.Auth.Register;
 
-internal class RegisterUserCommandValidator : ValidationBehavior<RegisterUserCommand> {
+internal class RegisterUserCommandValidator : ValidationBehavior<RegisterUserCommand>
+{
     private readonly IUsersRepository _users;
-    
-    public RegisterUserCommandValidator(IUsersRepository users) {
-        RuleFor(x => x.Login).NotEmpty();
+
+    public RegisterUserCommandValidator(IUsersRepository users)
+    {
+        RuleFor(x => x.Login).NotEmpty().MinimumLength(1).MaximumLength(255);
         RuleFor(x => x.Password).NotEmpty().MinimumLength(8).MaximumLength(255)
             .Matches(new Regex(@"^[a-zA-Z0-9!@#\$%\^&*\.\(\)]*$"))
             .WithMessage("Password should contain latin alphabet letters (a–z и A–Z), digits (0–9) and special characters !@#$%^&*.()");
         _users = users;
     }
 
-    protected override async Task<Result> RequestValidateAsync(RegisterUserCommand command, CancellationToken ct) {
+    internal override async Task<Result> RequestValidateAsync(RegisterUserCommand command, CancellationToken ct)
+    {
         var user = await _users.GetByLogin(command.Login, ct);
-        if (user != null) {
+        if (user != null)
+        {
             return Result.Failure("User with this login already exists");
         }
-        
+
         return await base.RequestValidateAsync(command, ct);
     }
 }
